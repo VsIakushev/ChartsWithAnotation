933f41c6f519be9a5c5e0d669292101dc21e5803
Vitalii Iakushev <vsiakushev@gmail.com>
2023-02-24 17:18:00 +0400

    Initial Commit

---
+++ ChartsWithAnotation/ChartsWithAnotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsWithAnotationApp.swift
+//  ChartsWithAnotation
+//
+//  Created by Vitaliy Iakushev on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChartsWithAnotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
